1e10966114a9247b14a9f872e4b5caab73c9aab7
Adelina <adeesa32@gmail.com>
2022-04-10 21:02:44 -0500

    Entregando después de ejecutar, falta el 9

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,6 @@
+// node main.js
+
+const logger = require('./logger')  //manda a llamar a .logger con la palabra require
+
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
