9d124991556e6955338e146fcfeb7f15ffc03b77
Adelina <adeesa32@gmail.com>
2022-04-11 14:25:33 -0500

    version 2

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,5 @@
+// crea la clase que necesitas para correr el archivo main.js
+// (debes exportarla usando ESM):
+//- usar export default
+//- la clase tiene la misma implementación que el ejercicio anterior
+//- ejecutar node.js
---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,9 @@
+let Pokemon = new Object(); // crea un objeto 
+Pokemon.tipo='mounstruo'
+
+//exportar usando CommonJS:
+//- Crea la clase
+//- Crea un constructor que permita recibir un nombre
+//- Crea la función sayHello()
+//- Creo la función sayMessage()
+//- Exporta la clase para que pueda ser importada en el archvio main.js
