1fc39cbea254684a21877440f59c7f06699e6a47
Adelina <adeesa32@gmail.com>
2022-04-15 17:30:31 -0500

    agregando ejercicio9

--- weekly_mission_1/example9/pokemon.js
+++ weekly_mission_1/example9/pokemon.js
@@ -1,5 +1,4 @@
-let Pokemon = new Object(); // crea un objeto 
-Pokemon.tipo='mounstruo'
+
 
 //exportar usando CommonJS:
 //- Crea la clase
@@ -7,3 +6,19 @@ Pokemon.tipo='mounstruo'
 //- Crea la función sayHello()
 //- Creo la función sayMessage()
 //- Exporta la clase para que pueda ser importada en el archvio main.js
+
+class Pokemon {
+    constructor (name) {
+      this.name = name
+    }
+    sayHello () {
+      console.log(`Mi pokemon ${this.name} te saluda!!!`);
+    }
+    sayMessage (message) {
+      console.log (`Mi pokemon ${this.name} dice: ${message}`);
+    }
+}
+
+//let Pokemon = new Object(); // crea un objeto 
+//    Pokemon.tipo='mounstruo'
+module.exports= Pokemon
